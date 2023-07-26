a46ea1fad59d304de056aef0107102d4975a7f44
grahamcottridge <codinggc@gmail.com>
2023-07-26 12:24:17 +0300

    Add selectors

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// unorderd list of player's quessed letters
+const guessedLetters = document.querySelector(".guessed-letters");
+// guess button
+const button = document.querySelector("button");
+// text input from player
+const playerInput = document.querySelector("input");
+// display of word in progress
+const wordInProgress = document.querySelector(".word-in-progress");
+// display remaining guesses
+const remainingText = document.querySelector(".remaining");
+// number of guesses span
+const remainingNum = document.querySelector(".remaining span");
+// message to display result of player guess
+const message = document.querySelector(".message");
+// play again button
+const playAgain = document.querySelector(".play-again");
+
