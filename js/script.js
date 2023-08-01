b66f51cdc2608be0544336c6077d049d89a3ac61
grahamcottridge <codinggc@gmail.com>
2023-08-01 22:28:22 +0300

    Add accept and validate player guess funcations

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // unorderd list of player's quessed letters
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedLetterList = document.querySelector(".guessed-letters");
 // guess button
 const button = document.querySelector("button");
 // text input from player
@@ -17,14 +17,13 @@ const playAgain = document.querySelector(".play-again");
 
 // starting word
 const word = "Magnolia";
-// array of guessed letters inputted
-const guessedLetter = []
+// array of guessed letters inputed
+const guessedLetters = [];
 
-// function to add placeholders to each letter
+// function to add placeholders for each letter
 const placeholder = function (word) {
   const placeholderLetters = [];
   for (const letter of word) {
-    // console.log(letter);
     placeholderLetters.push("●");
   }
   wordInProgress.innerText = placeholderLetters.join("");
@@ -36,24 +35,41 @@ placeholder(word);
 const validatePlayerInput = function (input) {
   const acceptedLetter = /[a-zA-Z]/;
   if (input === "") {
-    return "Please enter a letter!";
+    message.innerText = "Please enter a letter!";
   } else if (input.length > 1) {
-    return "Please only enter one letter at a time!";
+    message.innerText = "Please only enter one letter at a time!";
   } else if (!input.match(acceptedLetter)) {
-    return "That's not a letter between A to Z!";
+    message.innerText = "That's not a letter between A to Z!";
   } else {
     return input;
   }
 };
-// console.log(checkPlayerInput("t"));
+
+const makeGuess = function (letter) {
+  // convert input to uppercase
+  const guess = letter.toUpperCase();
+  // check letter not already guessed / else push to array
+  if (guessedLetters.includes(guess)) {
+    message.innerText = "You've already guessed that letter - try again!";
+  } else {
+    guessedLetters.push(guess);
+    console.log(guessedLetters);
+  }
+};
 
 // Button event handler
 button.addEventListener("click", function (e) {
   e.preventDefault();
+  // clear message
+  message.innerText = "";
+  // assign input value
   const input = playerInput.value;
-  const guess = validatePlayerInput(input)
+  // run validation function and assign to variable
+  const guess = validatePlayerInput(input);
+  // if validated run makeGuess function
+  if (guess) {
+    makeGuess(guess);
+  }
+  // clear input field
   playerInput.value = "";
-  console.log(guess)
 });
-
-
